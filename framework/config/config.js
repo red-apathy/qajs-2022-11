122107d8df5f84e865ab893a5a230b42744219f3
azubkov93 <46124676+azubkov93@users.noreply.github.com>
2023-05-21 17:17:11 +0300

    new test user

--- framework/config/config.js
+++ framework/config/config.js
@@ -1,8 +1,9 @@
 const config = {
     baseURL: 'https://bookstore.demoqa.com',
     credentials: {
-        'userName': 'test18881',
-        'password': 'Test!0test'
+        'userName': 'test11112',
+        'password': 'Test!0test',
+        'userId': '4b25bff2-3b89-443b-bb98-9c0dbe406df3'
     }
 }
 
